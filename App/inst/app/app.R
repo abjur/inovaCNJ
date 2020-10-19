92cf87cf79e32f3cbf5ba52c55fa06145abadd75
Julio Trecenti <julio.trecenti@gmail.com>
2020-10-19 14:35:51 -0300

    a0_info

--- App/inst/app/app.R
+++ App/inst/app/app.R
@@ -1,6 +1,8 @@
 # shiny::shinyApp(inovaCNJ:::app_ui(), inovaCNJ:::app_server)
 readRenviron(".Renviron")
 
-library(auth0)
-
-auth0::shinyAppAuth0(inovaCNJ:::app_ui(), inovaCNJ:::app_server)
+a0_info <- auth0::auth0_info(auth0::auth0_config("_auth0.yml"))
+shiny::shinyApp(
+  auth0::auth0_ui(inovaCNJ:::app_ui(), a0_info),
+  auth0::auth0_server(server, a0_info)
+)
