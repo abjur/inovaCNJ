af961f3af8a43c611e4dd3a2281dac2a615b145d
Julio Trecenti <julio.trecenti@gmail.com>
2021-01-19 19:58:21 -0300

    remove authentication temporarily

--- App/inst/app/app.R
+++ App/inst/app/app.R
@@ -1,4 +1,4 @@
-# shiny::shinyApp(inovaCNJ:::app_ui(), inovaCNJ:::app_server)
-library(inovaCNJ)
-readRenviron(".Renviron")
-auth0::shinyAppAuth0(inovaCNJ:::app_ui(), inovaCNJ:::app_server)
+shiny::shinyApp(inovaCNJ:::app_ui(), inovaCNJ:::app_server)
+# library(inovaCNJ)
+# readRenviron(".Renviron")
+# auth0::shinyAppAuth0(inovaCNJ:::app_ui(), inovaCNJ:::app_server)
